c33e2617196fd8ac91aec68098a4d79cec9294d0
Katzenbiber <wrobel1999@gmail.com>
2017-03-11 15:28:38 +0100

    Fixed Sidebar
    Made Navigationbar likeable

--- animation.js
+++ animation.js
@@ -2,24 +2,10 @@
 var isOpen = false;
 
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
-    if (screen.width / window.innerWidth > 2) {
-        document.getElementById("main").style.marginLeft = "150px";
-    }
+    document.getElementById("mySidenav").style.width = "350px";
     document.getElementById("myclosebtn").style.animation = "rotation 0.5s cubic-bezier(.45, 0, .55, 1) 1";
     document.getElementById("myclosebtn").style.transform = "rotate(90deg) translate(-12px, -12px)";
-    /*myclosebtn.style.transform = "rotate(90deg) translate(-12px)";
-    var closebtn = document.getElementsById('myclosebtn');
-    var rot = 0;
-    var id = setInterval(frame, 5);
-    function frame() {
-        if (pos == 90) {
-            clearInterval(id);
-        } else {
-            rot++;
-            closebtn.style.transform = 'rotate(' + rot + 'deg) translate(' + rot / 12 + ');';
-        }
-    }*/
+    $( "div:hidden" ).show();
     if (isOpen === true) {
         closeNav();
         return;
@@ -30,8 +16,8 @@ function openNav() {
 /* Set the width of the side navigation to 0 and the left margin of the page content to 0 */
 function closeNav() {
     document.getElementById("mySidenav").style.width = "100px";
-    document.getElementById("main").style.marginLeft = "0";
     document.getElementById("myclosebtn").style.animation = "rotationReverse 0.5s cubic-bezier(.45, 0, .55, 1) 1";
     document.getElementById("myclosebtn").style.transform = "rotate(0deg) translate(0px, 0px)";
+    $("#SidebarLinks").hide("fast");
     isOpen = false;
 }
