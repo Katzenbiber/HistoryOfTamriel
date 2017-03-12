cbc8a20f0359c9d863266cd98aa4ad1cdb22cdab
David Berger <David Berger>
2017-03-12 15:57:34 +0100

    sachen

---
+++ Tools/Wiki2Timeline.py
@@ -0,0 +1,32 @@
+import re
+inputpath = "input.txt"
+outputpath = "output.txt"
+def hilfe(matchobj):
+    tmp=re.sub(r'.*>(.+?)<.*',r'\1',matchobj.group(0),flags=re.DOTALL)
+    return ""
+file = open(inputpath,"r")
+string = file.read()
+file.close()
+print("von " + inputpath + " gelesen")
+string = string.replace("/wiki","http://elderscrolls.wikia.com/wiki")
+print("links globalisiert")
+string = string.replace("<ul>","</li>")
+string = string.replace("</ul>","")
+string = re.sub(r'<li>(.*?)</li>',r'<li>\n\t<div class="timeline-panel">\n\t\t<div class="tl-body">\n\t\t\t<p>\n\t\t\t\t\1\t\t\t</p>\n\t\t</div>\n\t</div>\n</li>\n\n',string,flags=re.DOTALL)
+print("bodies übersetzt")
+string = re.sub(r'<h2><span class="mw-headline" id="[^"]*?">(.*?)</span></h2>',r'<li><div class="tldate">\1</div></li>',string,flags=re.DOTALL)
+string = re.sub(r'<h3><span class="mw-headline" id="[^"]*?">(.*?)</span></h3>',r'',string,flags=re.DOTALL)
+string = re.sub(r'<h4><span class="mw-headline" id="[^"]*?">(.*?)</span></h4>',hilfe,string,flags=re.DOTALL)
+string = re.sub(r'<h4><span id="[^"]*?" class="mw-headline">(.*?)</span></h4>',hilfe,string,flags=re.DOTALL)
+print("headlines übersetzt")
+string = re.sub(r'<p><b>.*?</b>.*?</p>',r'',string,flags=re.DOTALL)
+print("Kommentare entfernt")
+string = re.sub(r'<p>Sources.*?</p>','',string,flags=re.DOTALL)
+string = re.sub(r'<sup.*?class="reference".*?</sup>',r'',string,flags=re.DOTALL)
+print("sources/Einzelnachweise entfernt")
+string = re.sub(r'</li>[' "\n " r']*?</li>',r'</li>\n\n',string,flags=re.DOTALL)
+print("cleanup abgeschlossen")
+file = open(outputpath,"w")
+file.write(string)
+file.close()
+print("in " + outputpath  + " gespeichert")
