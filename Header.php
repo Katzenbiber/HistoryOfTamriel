d9fcb87c44c74999ed0ef2f6bdf39c49a55b29a2
Katzenbiber <wrobel1999@gmail.com>
2017-03-14 19:48:09 +0100

    Changed index from html to php
    Additionally outsourced Sidebar and Header

---
+++ Header.php
@@ -0,0 +1,6 @@
+<?php
+echo
+    '<li><a href="/index.php" class="logo">The History of Tamriel</a></li>
+        <li><a href="/" class="TopNavBarButton">Map</a></li>
+        <li><a href="about.html" class="TopNavBarButton">About</a></li>';
+?>
